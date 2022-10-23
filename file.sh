685fdf1e525aa14362cd212be2d5f9713882b887
WhiteH4T-Dev <83751620+WhiteH4T-Dev@users.noreply.github.com>
2022-10-24 09:43:20 +1000

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,4 +1,17 @@
-fdisk -l 
-echo ""
-read -p "Drive: " DRIVE
-(echo m; echo g; echo n; ... ; ... ; echo +550M; echo n; echo 2; ... ; echo +2G; echo t; echo L; echo 1) | fdisk $DRIVE
+
+read -p "AAA: " drive
+fdisk $drive << EOF
+m
+g
+n
+
+
++550M
+n
+2
+
++2G
+t
+L
+1
+EOF
