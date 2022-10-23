cd571bc7aee433f80de9f577208a5704577f81dd
WhiteH4T-Dev <83751620+WhiteH4T-Dev@users.noreply.github.com>
2022-10-24 09:31:15 +1000

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,3 +1,4 @@
 fdisk -l 
+echo ""
 read -p "Drive: " DRIVE
-(echo m; echo g; echo n; ...; ...; echo +550M; echo n; echo 2; ...; echo +2G; echo t; echo L; echo 1) | fdisk $DRIVE
+(echo m; echo g; echo n; ... ; ... ; echo +550M; echo n; echo 2; ... ; echo +2G; echo t; echo L; echo 1) | fdisk $DRIVE
