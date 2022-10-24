3c5190aec81c2a8a103ac5be7595104a4d4b9795
WhiteH4T-Dev <83751620+WhiteH4T-Dev@users.noreply.github.com>
2022-10-24 10:49:19 +1000

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,17 +1,20 @@
-
-read -p "AAA: " drive
-fdisk $drive << EOF
-m
+#Partition your Disks
+lsblk
+read -p "Disk: " A1
+fdisk $A1 << EOF
 g
 n
 
 
-+550M
-n
-2
-
-+2G
++500M
 t
 L
 1
+n
+2
+
+
+w
 EOF
+clear
+lsblk
